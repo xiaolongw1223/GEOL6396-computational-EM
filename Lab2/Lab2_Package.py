34fc9357e1fa67527d62d01c20f958c5fcfe758d
xiaolongw1223 <56940558+xiaolongw1223@users.noreply.github.com>
2019-10-30 10:09:54 -0500

    upload Lab2 python code

---
+++ Lab2/Lab2_Package.py
@@ -0,0 +1,496 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 26 16:38:53 2019
+
+@author: wxl
+"""
+
+import numpy as np
+import scipy.sparse as sp
+import time
+
+norm = np.linalg.norm
+
+
+# =============================================================================
+#
+# Author: Xiaolong Wei, 10/26/2019, at University of Houston 
+#
+# Based on the Dr.Jiajia Sun's matlab code.
+#
+# This package is flexible and it can be applied to 
+# any geophysical inversion problem -- 
+#
+# Mesh (finished)
+# Discretize (finished)
+# 
+# =============================================================================
+
+
+class Mesh(object):
+    
+    '''
+    
+    In this mesh class, we can do:
+        
+    1. calcualte width of cells in x, y, z directions. eg: Mesh.WCx
+    2. calculate center of cells in x, y, z directions. eg: Mesh.CCx
+    3. calculate number of cells in x, y, z directions. eg: Mesh.nCx
+    4. calculate total number of cells. eg: Mesh.nC
+    5. calculate mod in x, y, z directions. eg: Mesh.Modx
+    
+    '''
+
+
+    def __init__(self, listx, listy, listz, **kwargs):
+        
+        '''
+        
+        Here, we use right hand coordinate system:
+        x: east
+        y: north
+        z: depth
+        
+        
+        
+        
+        listx = [ [CoreModelVectorX], Lx, Px ]
+        
+        where, 
+        CoreModelVectorX : core mesh area in x direction
+        Lx: maximum length in east
+        Px: power number of padding cells (exponent)
+        
+        
+        
+        
+        
+        listy, listz is totally same with listx
+        
+        
+        
+        '''
+
+        self.CoreModelVectorX = listx[0]
+        self.CoreModelVectorY = listy[0]
+        self.CoreModelVectorZ = listz[0]
+
+        self.Lx = listx[1]
+        self.Ly = listy[1]
+        self.Lz = listy[1]
+        
+        self.Px = listx[-1]
+        self.Py = listy[-1]
+        self.Pz = listy[-1]
+        
+
+    @ property
+    def WCx(self):
+        '''width of cells in x direction'''
+        
+        if isinstance(self.CoreModelVectorX, (np.ndarray)):
+            T = self.CoreModelVectorX.tolist()
+        
+        
+        assert isinstance(T, (list)), "the core vector should be a list"
+        
+        
+        while 1:
+            T.insert(0, T[0] * self.Px)
+            T.insert(len(T), T[-1] * self.Px)
+
+            if np.sum(T) > self.Lx:
+                break   
+        
+        return np.array(T).flatten('F')
+    
+    @ property
+    def WCy(self):
+        '''width of cells in y direction'''
+        
+        if isinstance(self.CoreModelVectorY, (np.ndarray)):
+            T = self.CoreModelVectorY.tolist()
+        
+        
+        assert isinstance(T, (list)), "the core vector should be a list"
+        
+        
+        while 1:
+          
+            T.insert(0, T[0] * self.Py)
+            T.insert(len(T), T[-1] * self.Py)
+
+            if np.sum(T) > self.Ly:
+                break   
+        
+        return np.array(T).flatten('F')
+    
+    @ property
+    def WCz(self):
+        '''width of cells in z direction'''
+        
+        if isinstance(self.CoreModelVectorZ, (np.ndarray)):
+            T = self.CoreModelVectorZ.tolist()
+        
+        
+        assert isinstance(T, (list)), "the core vector should be a list"
+        
+        
+        while 1:
+          
+            T.insert(len(T), T[-1] * self.Pz)
+
+            if np.sum(T) > self.Lz:
+                break   
+        
+        return np.array(T).flatten('F')
+    
+    
+    @ property
+    def nCx(self):
+        '''number of cells in x direction'''
+        
+        return len(self.WCx)
+    
+    @ property
+    def nCy(self):
+        '''number of cells in y direction'''
+        
+        return len(self.WCy)
+    
+    @ property
+    def nCz(self):
+        '''number of cells in z direction'''
+        
+        return len(self.WCz)
+    
+    @ property
+    def nC(self):
+        
+        return self.nCx * self.nCy * self.nCz
+    
+    @ property
+    def Modx(self):
+
+        xmod = []
+        xmod.append(0)
+
+        for i in range(self.nCx):
+
+            xmod.append(xmod[i] + self.WCx[i])
+
+        return np.array(xmod)
+    
+    
+    @ property
+    def Mody(self):
+        '''center point of cells in x direction'''
+        
+        ymod = []
+        ymod.append(0)
+        
+        for i in range(self.nCy):
+            
+            ymod.append(ymod[i] + self.WCy[i])
+        
+        return np.array(ymod)
+        
+        
+    @ property
+    def Modz(self):
+        '''center point of cells in x direction'''
+        
+        zmod = []
+        zmod.append(0)
+        
+        for i in range(self.nCz):
+            
+            zmod.append(zmod[i] + self.WCz[i])
+
+        return np.array(zmod)
+    
+    @ property
+    def CCx(self):
+        '''center point of cells in x direction'''
+        
+        xmid = []
+        for i in range(self.nCx):
+            
+            xmid.append(0.5 * (self.Modx[i] + self.Modx[i+1]))
+        
+        return np.array(xmid)
+    
+    
+    @ property
+    def CCy(self):
+        '''center point of cells in x direction'''
+        
+        ymid = []
+        for i in range(self.nCy):
+            
+            ymid.append(0.5 * (self.Mody[i] + self.Mody[i+1]))
+        
+        return np.array(ymid)
+        
+        
+    @ property
+    def CCz(self):
+        '''center point of cells in x direction'''
+        
+        zmid = []
+        for i in range(self.nCz):
+
+            zmid.append(0.5 * (self.Modz[i] + self.Modz[i+1]))
+        
+        return np.array(zmid)
+        
+        
+        
+        
+
+    
+class Discretize(Mesh): 
+    '''
+    This class includes:
+    
+    1. getMeshGeometry functioin: volumn of cells, and area of faces.
+    
+    2. finite difference matrix in x, y, z directions.
+    
+    '''
+    def __init__(self, listx, listy, listz, **kwargs):
+    
+        self.as_super = super(Discretize, self)
+        self.as_super.__init__(listx, listy, listz, **kwargs) # pass the mesh to the class Mesh
+    
+    
+    @ property
+    def getMeshGeometry(self):
+        
+        '''
+        V is a diagonal matrix that has the cell volumes on its diagonal, 
+        F is also a diagonal matrix with the areas of all faces on its diagonal. 
+        L is also a diagonal matrix with the length of all edges on its diagonal.
+        
+        '''
+        h1 = self.WCz
+        h2 = self.WCx
+        h3 = self.WCy
+        
+        n1 = len(h1) 
+        n2 = len(h2) 
+        n3 = len(h3)
+        
+        V = sp.kron(sp.diags(h3), sp.kron(sp.diags(h2), sp.diags(h1)))
+        
+        F1 = sp.kron(sp.diags(h3), sp.kron(sp.diags(h2), sp.eye(n1+1)))
+        F2 = sp.kron(sp.diags(h3), sp.kron(sp.eye(n2+1), sp.diags(h1)))
+        F3 = sp.kron(sp.eye(n3+1), sp.kron(sp.diags(h2), sp.diags(h1)))
+        
+        # extract diag elements and make a long vector
+        F = np.r_[np.diag(F1.A), np.diag(F2.A), np.diag(F3.A)]
+        F = sp.diags(F)
+    
+        
+        L1 = sp.kron(sp.eye(n3+1), sp.kron(sp.eye(n2+1), sp.diags(h1)))
+        L2 = sp.kron(sp.eye(n3+1), sp.kron(sp.diags(h2), sp.eye(n1+1)))
+        L3 = sp.kron(sp.diags(h3), sp.kron(sp.eye(n2+1), sp.eye(n1+1)))
+        
+        L = np.r_[np.diag(L1.A), np.diag(L2.A), np.diag(L3.A)]
+        L = sp.diags(L)
+        
+        return [V, F, L]
+        
+  
+    
+    def Diff(self, direc = 'x'):
+        
+        '''
+        In forward finite difference matrix:
+        
+        X = east = 2
+        Y = north = 3
+        Z = depth = 1
+        
+        I is identity matrix
+        
+        D is 1D finite difference matrix
+        
+        '''
+        Directions = ['x', 'y', 'z']
+        
+        self.direc = direc
+        assert direc in Directions,  "direction not valid!"
+        
+        
+        Iz = sp.eye(self.nCz)
+        Ix = sp.eye(self.nCx)
+        Iy = sp.eye(self.nCy)
+        
+        
+        if direc == 'x':
+            e = np.ones(self.nCx)
+            Dx = sp.spdiags([-e, e], [0, 1], self.nCx, self.nCx+1)
+            D = sp.kron(sp.kron(Iy, Dx), Iz)
+        
+        
+        if direc == 'y':
+            e = np.ones(self.nCy)
+            Dy = sp.spdiags([-e, e], [0, 1], self.nCy, self.nCy+1)
+            D = sp.kron(sp.kron(Dy, Ix), Iz)
+        
+        
+        if direc == 'z':
+            e = np.ones(self.nCz)
+            Dz = sp.spdiags([-e, e], [0, 1], self.nCz, self.nCz+1)
+            D = sp.kron(sp.kron(Iy, Ix), Dz)
+        
+        return D
+    
+    
+    
+    
+    def Diff2(self, direc = 'x'):
+        
+        '''
+        In forward finite difference matrix:
+        
+        X = east = 2
+        Y = north = 3
+        Z = depth = 1
+        
+        I is identity matrix
+        
+        D is 1D finite difference matrix
+        
+        '''
+        Directions = ['x', 'y', 'z']
+        
+        self.direc = direc
+        assert direc in Directions,  "direction not valid!"
+        
+        
+        Iz = sp.eye(self.nCz)
+        Ix = sp.eye(self.nCx)
+        Iy = sp.eye(self.nCy)
+        
+        
+        if direc == 'x':
+            e = np.ones(self.nCx) * 0.5
+            Dx = sp.spdiags([e, e], [0, 1], self.nCx, self.nCx+1)
+            D = sp.kron(sp.kron(Iy, Dx), Iz)
+        
+        
+        if direc == 'y':
+            e = np.ones(self.nCy) * 0.5
+            Dy = sp.spdiags([e, e], [0, 1], self.nCy, self.nCy+1)
+            D = sp.kron(sp.kron(Dy, Ix), Iz)
+        
+        
+        if direc == 'z':
+            e = np.ones(self.nCz)* 0.5
+            Dz = sp.spdiags([e, e], [0, 1], self.nCz, self.nCz+1)
+            D = sp.kron(sp.kron(Iy, Ix), Dz)
+        
+        return D
+
+
+
+
+
+
+class Utils(Mesh):
+    
+    '''
+    This class includes:
+        
+    1. SourceCorrection function
+    2. Conjugate gradient
+    
+    '''
+    def __init__(self, listx, listy, listz, **kwargs):
+        
+        self.as_super = super(Utils, self)
+        self.as_super.__init__(listx, listy, listz, **kwargs) # pass the mesh to the class Mesh
+    
+
+        
+    def CG(self, A, b):
+        
+        t = time.time()
+        
+        tol = 1e-3
+        maxIter = 5000
+        x = 0 * b
+        pvec = A.dot(x)
+        
+        r = b - pvec
+        p = r
+        rhop = r.T.dot(r)
+        
+        bnorm = norm(b)
+        
+        for i in range(maxIter):
+            
+            q = A.dot(p)
+            alpha = rhop / (p.T.dot(q))
+            x = x + alpha * p
+            r = r - alpha * q
+            
+            rcg = norm(r)/bnorm
+            if rcg < tol:
+                break
+            
+            rho = r.T.dot(r)
+            p = r + (rho/rhop) * p
+            
+            rhop = rho
+        
+        print("CG solver time", time.time() - t)    
+        return x
+        
+     
+            
+    '''This function code is borrowed from Felicia'''
+    def SourceCorrection(self, A, sigma_H, I, PositiveElectrode_Loc, NegativeElectrode_Loc):
+        
+        rho = 1 / sigma_H
+
+        xmod = self.Modx
+        ymod = self.Mody
+        zmod = self.Modz
+    
+        phi_true = np.zeros((self.nCz, self.nCx, self.nCy))
+        phi_true_vec = np.zeros(self.nCz * self.nCx * self.nCy)
+
+        for i in range(self.nCy):
+            
+            xloc = (xmod[i]+ xmod[i+1])/2.
+            
+            for j in range(self.nCx):
+                
+                yloc = (ymod[j] + ymod[j+1])/2.
+                
+                for k in range(self.nCz):         
+                    
+                    zloc = (zmod[k] + zmod[k+1])/2.;
+                    CellLoc = np.transpose([zloc,yloc,xloc])# the coordinates of the center of a cell
+                    
+                    distanceA = np.linalg.norm(CellLoc - PositiveElectrode_Loc)
+                    distanceB = np.linalg.norm(CellLoc - NegativeElectrode_Loc)
+                    
+                    phi_true[k,j,i] = rho*I*(1./distanceA - 1./distanceB)/(2*np.pi)
+
+        phi_true_vec = phi_true.flatten('F')
+        q_corr = A*phi_true_vec
+    
+    
+    
+        return(q_corr)
+        
+            
+        
+        
+        
+        
---
+++ Lab2/Lab2_xiaolong.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 26 16:59:12 2019
+
+@author: wxl
+"""
+import scipy.io as sio
+import numpy as np
+import scipy.sparse as sp
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d # unused import
+import time
+import datetime
+import Lab2_Package
+
+
+def run(plotIt=True):
+
+    t = time.time()
+    
+    hxind = [0.75*np.ones(60), 3*45, 1.2]
+    hyind = [0.75*np.ones(60), 3*45, 1.2]
+    hzind = [0.75*np.ones(60), 3*45, 1.2]
+    
+
+    Mesh = Lab2_Package.Mesh(hxind, hyind, hzind)
+    U = Lab2_Package.Utils(hxind, hyind, hzind)
+
+    '''location of positive current electrode: 0 m in depth, 64 m in easting and 76 m in northing.'''
+    SourceLocation3D_postive_Loc = [0, 64, 76]
+    
+    '''location of negative current electrode: 0 m in depth, 87 m in easting and 76 m in northing.'''
+    SourceLocation3D_negative_Loc = [0, 87, 76]
+    
+    
+    sigma = np.ones([Mesh.nCz, Mesh.nCx, Mesh.nCy]) * 0.01
+    sigma2 = sigma.copy('F')
+    sigma2[4:20, 39:50, 39:50] = 0.1
+    
+    sigma = sigma.flatten('F')
+    sigma2 = sigma2.flatten('F')  # inhomo
+    
+    Discre = Lab2_Package.Discretize(hxind, hyind, hzind)
+    [V, F, L] = Discre.getMeshGeometry
+    print("V.shape", V.shape)
+    print("F.shape", F.shape)
+
+
+    Div = sp.hstack([Discre.Diff(direc='z'), Discre.Diff(direc='x'), Discre.Diff(direc='y')])
+    print("Div.shape", Div.shape)
+    
+
+    V_inv = sp.diags(1/np.diag(V.A))
+    D = V_inv.dot(Div).dot(F)
+    print("D.shape", D.shape)
+
+
+    Afc = sp.hstack([Discre.Diff2(direc='z'), Discre.Diff2(direc='x'), Discre.Diff2(direc='y')])
+    print("Afc.shape", Afc.shape)
+    
+
+    Mf = Afc.T.dot(V.dot(1/sigma))
+    Mf2 = Afc.T.dot(V.dot(1/sigma2)) # inhomo
+    print("Mf.shape", Mf.shape)
+    print("Mf.type", type(Mf))
+
+
+    Mf_inv = sp.diags(1/Mf)    
+    A = V.dot(D).dot(Mf_inv).dot(D.T).dot(V)
+    print("A.shape", A.shape)
+    
+
+    Mf_inv2 = sp.diags(1/Mf2)
+    A2 = V.dot(D).dot(Mf_inv2).dot(D.T).dot(V) #inhomo
+
+
+    sigma_H = 0.01
+    Mf_H = sp.diags(Afc.T.dot(V.dot(1/sigma_H * np.ones(Mesh.nC))))
+    print("Mf_H.shape", Mf_H.shape)
+    
+    Mf_H_inv = sp.diags(1/np.diag(Mf_H.A))
+    A_H = V.dot(D).dot(Mf_H_inv.dot(D.T.dot(V)))
+    print("A_H.shape", A_H.shape)
+    
+    
+    q_corr = U.SourceCorrection(A_H, sigma_H, 1,
+                                          SourceLocation3D_postive_Loc,
+                                          SourceLocation3D_negative_Loc)
+    print("q_corr.shape", q_corr.shape)
+    
+
+    phi = U.CG(A, q_corr)
+    phi2 = U.CG(A2, q_corr)
+    diff = np.subtract(phi2, phi)
+    
+    
+    if plotIt:
+        
+        # figure 1 ----------------
+        [xx, yy] = np.meshgrid(Mesh.Mody, Mesh.Modx)
+        zz = np.zeros([len(yy), len(yy[0])])
+
+        fig = plt.figure()
+        ax = plt.gca(projection='3d')
+        ax.scatter([63, 86], [75, 75], [0, 0], marker='v', c = 'r', linewidth=2)
+        ax.plot_surface(xx, yy, zz)
+        ax.set_ylabel('Northing (m)')
+        ax.set_xlabel('Easting (m)')
+
+        
+        # figure 2 -----------------
+        phi3d = np.reshape(phi, [Mesh.nCz, Mesh.nCx, Mesh.nCy], 'F')
+        phi3d_surface = np.squeeze(phi3d[0, :, :]).T
+        extent = [Mesh.CCy[14], Mesh.CCy[73], Mesh.CCx[14], Mesh.CCx[73]]
+
+        fig = plt.figure()
+        ax = plt.gca()
+        im = ax.imshow(phi3d_surface[14:73, 14:73], cmap ='jet', extent = extent)
+        ax.set_xlabel('Easting (m)')
+        ax.set_ylabel('Northing (m)')
+        ax.set_title('Electrical potential at the surface')
+        fig.colorbar(im, ax = ax, orientation="vertical")
+        
+        
+        # figure 2-1 -----------------
+        phi3d_slice = np.squeeze(phi3d[:, :, 44])
+        extent = [Mesh.CCx[14], Mesh.CCx[73], Mesh.CCz[59], Mesh.CCz[0]]
+
+        fig = plt.figure()
+        ax = plt.gca()
+        im = ax.imshow(phi3d_slice[0:59, 14:73], cmap ='jet', extent = extent)
+        ax.set_xlabel('Easting (m)')
+        ax.set_ylabel('Depth (m)')
+#        ax.invert_yaxis()
+        ax.set_title('Electrical potential directly underneath current electrodes')
+        fig.colorbar(im, ax = ax, orientation="vertical")
+        
+        
+         # figure 3 -----------------
+        phi3d3 = np.reshape(diff, [Mesh.nCz, Mesh.nCx, Mesh.nCy], 'F')
+        phi3d_surface3 = np.squeeze(phi3d3[0, :, :]).T
+        extent = [Mesh.CCy[14], Mesh.CCy[73], Mesh.CCx[14], Mesh.CCx[73]]
+
+        fig = plt.figure()
+        ax = plt.gca()
+        im = ax.imshow(phi3d_surface3[14:73, 14:73], cmap ='jet', extent = extent)
+        ax.set_xlabel('Easting (m)')
+        ax.set_ylabel('Northing (m)')
+        ax.set_title('Secondary potential at the surface')
+        fig.colorbar(im, ax = ax, orientation="vertical")
+        
+        
+        # figure 3-1 -----------------
+        phi3d_slice3 = np.squeeze(phi3d3[:, :, 44])
+        extent = [Mesh.CCx[14], Mesh.CCx[73], Mesh.CCz[59], Mesh.CCz[0]]
+
+        fig = plt.figure()
+        ax = plt.gca()
+        im = ax.imshow(phi3d_slice3[0:59, 14:73], cmap ='jet', extent = extent)
+        ax.set_xlabel('Easting (m)')
+        ax.set_ylabel('Depth (m)')
+        ax.set_title('Secondary potential directly underneath current electrodes')
+        fig.colorbar(im, ax = ax, orientation="vertical")
+    
+    
+    elapse = time.time() - t
+    print("time: ", str(datetime.timedelta(seconds = elapse)))
+if __name__ == '__main__':
+    run()
+    plt.show()
+
